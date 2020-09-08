e768ee149462975c948f9ef3c802f1d768fd0c6c
Alex Grand <namelost@protonmail.com>
2020-09-08 20:18:31 +0300

    dijkstra algorithm

--- ch7/dijkstra_slgorithm.py
+++ ch7/dijkstra_slgorithm.py
@@ -22,3 +22,36 @@ parents['in'] = None
 
 processed = []
 
+def find_lowest_cost_node(costs):
+    lowest_cost = float('inf')
+    lowest_cost_node = None
+
+    for node in costs:
+        cost = costs[node]
+
+        if cost < lowest_cost and node not in processed:
+            lowest_cost = cost
+            lowest_cost_node = node
+    
+    return lowest_cost_node
+
+def dijkstra_search(graph, costs, parents):
+    node = find_lowest_cost_node(costs)
+    
+    while node is not None:
+        cost = costs[node]
+        neighbors = graph[node]
+        
+        for n in neighbors.keys():
+            new_cost = cost + neighbors[n]
+            
+            if costs[n] > new_cost:
+                costs[n] = new_cost
+                parents[n] = node
+        
+        processed.append(node)
+        node = find_lowest_cost_node(costs)
+
+
+dijkstra_search(graph, costs, parents)
+print(processed)
